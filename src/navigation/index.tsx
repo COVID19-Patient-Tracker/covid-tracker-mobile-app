ec4d838bf2646b705ccdc6377921ab74f730ea28
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-09-23 02:50:45 +0530

    Add more navigation support

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,26 +1,98 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import { Pressable } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../constants/navtypes';
 
 import {
   HomeScreen,
   LoginScreen,
   RegisterScreen,
-  UserHomeScreen,
   ForgotPasswordScreen,
+  NotFoundScreen,
+  UserHomeScreen,
+  UserProfileScreen,
+
 } from '../screens';
 
-const Router = createStackNavigator(
-  {
-    HomeScreen,
-    LoginScreen,
-    RegisterScreen,
-    UserHomeScreen,
-    ForgotPasswordScreen,
-  },
-  {
-    initialRouteName: 'HomeScreen',
-    headerMode: 'none',
-  }
-);
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
 
-export default createAppContainer(Router);
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Root" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={RegisterScreen} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+      <Stack.Screen name="UserRoot" component={BottomTabNavigator} options={{ headerShown: false }} />
+      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+    </Stack.Navigator>
+  );
+}
+
+const BottomTab = createBottomTabNavigator<RootTabParamList>();
+
+function BottomTabNavigator() {
+  //const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="UserHome"
+      screenOptions={{
+        //tabBarActiveTintColor: Colors[colorScheme].tint,
+      }}>
+      <BottomTab.Screen
+        name="UserHome"
+        component={UserHomeScreen}
+        options={({ navigation }: RootTabScreenProps<'UserHome'>) => ({
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerRight: () => (
+            <Pressable
+              onPress={() => navigation.navigate('Modal')}
+              style={({ pressed }) => ({
+                opacity: pressed ? 0.5 : 1,
+              })}>
+              <FontAwesome
+                name="info-circle"
+                size={25}
+                //color={Colors[colorScheme].text}
+                style={{ marginRight: 15 }}
+              />
+            </Pressable>
+          ),
+        })}
+      />
+      <BottomTab.Screen
+        name="UserProfile"
+        component={UserProfileScreen}
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,5 +1,7 @@
 export { default as HomeScreen } from './HomeScreen';
 export { default as LoginScreen } from './LoginScreen';
 export { default as RegisterScreen } from './RegisterScreen';
-export { default as UserHomeScreen } from './UserHomeScreen';
 export { default as ForgotPasswordScreen } from './ForgotPasswordScreen';
+export { default as NotFoundScreen } from './NotFoundScreen';
+export { default as UserHomeScreen } from './UserScreen/UserHomeScreen';
+export { default as UserProfileScreen } from './UserScreen/UserProfileScreen';
