bc7580f6c5bc18ea6d050af7e3789a4b8ba739b0
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2021-10-31 01:02:16 +0530

    Add login functionality

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -10,26 +10,66 @@ import { theme } from '../shared/theme'
 import { emailValidator, passwordValidator } from '../shared/utils';
 
 import { Navigation } from '../constants/types';
+import {API_URL} from '../shared/routes';
 
 type Props = {
     navigation: Navigation;
 };
 
 const LoginScreen = ({ navigation }: Props) => {
-    const [email, setEmail] = useState({ value: '', error: '' });
-    const [password, setPassword] = useState({ value: '', error: '' });
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailErr, setEmailErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+
+    const [isError, setIsError] = useState(false);
+    const [message, setMessage] = useState('');
+    const [isLogin, setIsLogin] = useState(true);
 
     const _onLoginPressed = () => {
-        const emailError = emailValidator(email.value);
-        const passwordError = passwordValidator(password.value);
+        const emailError = emailValidator(email);
+        const passwordError = passwordValidator(password);
 
         if (emailError || passwordError) {
-            setEmail({ ...email, error: emailError });
-            setPassword({ ...password, error: passwordError });
+            setEmailErr(emailError);
+            setPasswordErr(passwordError);
             return;
         }
 
-        navigation.navigate('UserRoot');
+        const payload = {
+            email,
+            password,
+        };
+
+        fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        })
+        .then(async res => { 
+            try {
+                const jsonRes = await res.json();
+                if (res.status !== 200) {
+                    console.log("Error occured");
+                    console.log(jsonRes);
+                    setIsError(true);
+                    setMessage(jsonRes.exception);
+                } else {
+                    //onLoggedIn(jsonRes.token);
+                    console.log("No error");
+                    setIsError(false);
+                    setMessage(jsonRes.exception);
+                    navigation.navigate('UserRoot');
+                }
+            } catch (err) {
+                console.log(err);
+            };
+        })
+        .catch(err => {
+            console.log(err);
+        });
     };
 
     return (
@@ -40,10 +80,15 @@ const LoginScreen = ({ navigation }: Props) => {
             <CustomTextInput
                 label="Email"
                 returnKeyType="next"
-                value={email.value}
-                onChangeText={text => setEmail({ value: text, error: '' })}
-                error={!!email.error}
-                errorText={email.error}
+                value={email}
+                onChangeText={text => {
+                    setEmail(text);
+                    setEmailErr('');
+                    setIsError(false);
+                    setMessage('');
+                }}
+                error={!!emailErr}
+                errorText={emailErr}
                 autoCapitalize="none"
                 autoCompleteType="email"
                 textContentType="emailAddress"
@@ -53,10 +98,15 @@ const LoginScreen = ({ navigation }: Props) => {
             <CustomTextInput
                 label="Password"
                 returnKeyType="done"
-                value={password.value}
-                onChangeText={text => setPassword({ value: text, error: '' })}
-                error={!!password.error}
-                errorText={password.error}
+                value={password}
+                onChangeText={text => {
+                    setPassword(text);
+                    setPasswordErr('');
+                    setIsError(false);
+                    setMessage('');
+                }}
+                error={!!passwordErr}
+                errorText={passwordErr}
                 secureTextEntry
             />
 
@@ -67,6 +117,8 @@ const LoginScreen = ({ navigation }: Props) => {
                     <Text style={styles.label}>Forgot your password?</Text>
                 </TouchableOpacity>
             </View>
+
+            {isError && <Text style={styles.errorText}>{message}</Text>}
 
             <CustomButton mode="contained" onPress={_onLoginPressed}>
                 Login
@@ -98,6 +150,10 @@ const styles = StyleSheet.create({
     link: {
         fontWeight: 'bold',
         color: theme.colors.primary,
+    },
+    errorText: {
+        fontWeight: 'bold',
+        color: theme.colors.error,
     },
 });
 
