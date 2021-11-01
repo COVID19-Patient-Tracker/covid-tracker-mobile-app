90639eb912f44d47b289f545aa910c1b603a9e66
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2021-11-02 00:33:02 +0530

    Update test pages

--- src/screens/UserScreen/AntigenTestScreen.tsx
+++ src/screens/UserScreen/AntigenTestScreen.tsx
@@ -1,9 +1,11 @@
-import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { memo, useState, useEffect } from 'react';
+import axios from "axios";
+import { StyleSheet, View, Text } from 'react-native';
 
 import TestResultCard from '../../components/UserScreen/TestResultCard';
 
-import { Test } from '../../constants/datatypes';
+import { Test, PcrTest } from '../../constants/datatypes';
+import { API_URL, test_acess_token } from '../../shared/routes';
 
 const dataSet = [
     {
@@ -21,12 +23,49 @@ const dataSet = [
 ]
 
 export default function AntigenTestScreen() {
+
+    const [antigenTestList, setAntigenTestList] = useState<Array<PcrTest>>([]);
+    const [message, setMessage] = useState('');
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        axios.get(`${API_URL}/management/api/V1/visitor/antigen/1`, {
+            headers: {
+                'Authorization': `Bearer ${test_acess_token}`
+            }
+        })
+            .then((res) => {
+                if (res.data.TestData.length == 0) {
+                    setIsError(true);
+                    setMessage("No Records to Display");
+                    console.log(message);
+                } else {
+                    for (let obj of res.data.TestData) {
+                        antigenTestList.push(obj);
+                    }
+                    console.log(antigenTestList);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                setIsError(true);
+                setMessage("An error Occured. Try Again Later");
+                console.log(message);
+            })
+    }, []);
+    
     return (
         <View style={styles.container}>
-            {dataSet.map((test : Test) => {
+            {isError &&
+                <View style={styles.info}>
+                    <Text style={styles.infoFont}>{message}</Text>
+                </View>
+            }
+            {antigenTestList.map((test: PcrTest) => {
                 return (
-                <TestResultCard key={test.id} testData={test} />
-            )})}
+                    <TestResultCard key={test.id} testData={test} />
+                )
+            })}
         </View>
     );
 }
@@ -36,5 +75,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-around',
         padding: 10,
+    },
+    info: {
+        backgroundColor: '#b6dddb',
+        width: "100%",
+        padding: 10,
+        marginBottom: 8,
+        marginTop: 8,
+    },
+    infoFont: {
+        fontSize: 17,
     },
 });
--- src/screens/UserScreen/PcrTestScreen.tsx
+++ src/screens/UserScreen/PcrTestScreen.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { StyleSheet, Button, Alert, View, Text } from 'react-native';
-
+import React, { memo, useState, useEffect } from 'react';
+import axios from "axios";
+import { StyleSheet, Alert, View, Text } from 'react-native';
 
 import TestResultCard from '../../components/UserScreen/TestResultCard';
 
-import { Test } from '../../constants/datatypes';
+import { Test, PcrTest } from '../../constants/datatypes';
+import { API_URL, test_acess_token } from '../../shared/routes';
 
 const dataSet = [
     {
@@ -15,14 +16,52 @@ const dataSet = [
     },
 ]
 
+
 export default function PcrTestScreen() {
+
+    const [pcrTestList, setPcrTestList] = useState<Array<PcrTest>>([]);
+    const [message, setMessage] = useState('');
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        axios.get(`${API_URL}/management/api/V1/visitor/pcr/2`, {
+            headers: {
+                'Authorization': `Bearer ${test_acess_token}`
+            }
+        })
+            .then((res) => {
+                if (res.data.TestData.length == 0) {
+                    setIsError(true);
+                    setMessage("No Records to Display");
+                    console.log(message);
+                } else {
+                    for (let obj of res.data.TestData) {
+                        pcrTestList.push(obj);
+                    }
+                    console.log(pcrTestList);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                setIsError(true);
+                setMessage("An error Occured. Try Again Later");
+                console.log(message);
+            })
+    }, []);
+
     return (
         <View style={styles.container}>
-            {dataSet.map((test: Test) => {
+            {isError &&
+                <View style={styles.info}>
+                    <Text style={styles.infoFont}>{message}</Text>
+                </View>
+            }
+            {pcrTestList.map((test: PcrTest) => {
                 return (
                     <TestResultCard key={test.id} testData={test} />
                 )
             })}
+
         </View>
     );
 }
@@ -32,5 +71,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-around',
         padding: 10,
+    },
+    info: {
+        backgroundColor: '#b6dddb',
+        width: "100%",
+        padding: 10,
+        marginBottom: 8,
+        marginTop: 8,
+    },
+    infoFont: {
+        fontSize: 17,
     },
 });
