f2ccb1f170067d02a2ee29d8f7b4538b581fd389
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2021-11-06 15:59:04 +0530

    Update routes

--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -1,6 +1,6 @@
- export const API_URL =  'http://localhost:8989';
+ //export const API_URL =  'http://localhost:8989';
 
- //export const API_URL =  'https://cpt-spring-app.azurewebsites.net';
+ export const API_URL =  'https://cpt-spring-app.azurewebsites.net';
 
  export const UPLOAD_XRAY_URL = "https://pneumonia-prediction-sep.herokuapp.com/predict";
 
