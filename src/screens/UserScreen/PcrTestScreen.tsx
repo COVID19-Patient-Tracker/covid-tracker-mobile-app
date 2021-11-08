756217f7fd4cb8e5ecee4c55c5c4bdb9f3a67340
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2021-11-08 09:24:23 +0530

    Retreive pcr tests from database

--- src/screens/UserScreen/PcrTestScreen.tsx
+++ src/screens/UserScreen/PcrTestScreen.tsx
@@ -1,10 +1,10 @@
-import React, { memo, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import { StyleSheet, Alert, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 
 import TestResultCard from '../../components/UserScreen/TestResultCard';
 
-import { Test, PcrTest } from '../../constants/datatypes';
+import { PcrTest } from '../../constants/datatypes';
 import { API_URL, test_acess_token } from '../../shared/routes';
 
 const dataSet = [
@@ -24,47 +24,47 @@ const dataSet = [
     },
 ]
 
-
 export default function PcrTestScreen() {
 
     const [pcrTestList, setPcrTestList] = useState<Array<PcrTest>>([]);
     const [message, setMessage] = useState('');
     const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-    // useEffect(() => {
-    //     axios.get(`${API_URL}/management/api/V1/visitor/pcr/2`, {
-    //         headers: {
-    //             'Authorization': `Bearer ${test_acess_token}`
-    //         }
-    //     })
-    //         .then((res) => {
-    //             if (res.data.TestData.length == 0) {
-    //                 setIsError(true);
-    //                 setMessage("No Records to Display");
-    //                 console.log(message);
-    //             } else {
-    //                 for (let obj of res.data.TestData) {
-    //                     pcrTestList.push(obj);
-    //                 }
-    //                 console.log(pcrTestList);
-    //             }
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //             setIsError(true);
-    //             setMessage("An error Occured. Try Again Later");
-    //             console.log(message);
-    //         })
-    // }, []);
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get(`${API_URL}/management/api/V1/visitor/pcr/2`, {
+            headers: {
+                'Authorization': `Bearer ${test_acess_token}`
+            }
+        })
+            .then((res) => {
+                setIsLoading(false);
+                if (res.data.TestData.length == 0) {
+                    setIsError(true);
+                    setMessage("No Records to Display");
+                } else {
+                    console.log(res.data.TestData);
+                    setPcrTestList(res.data.TestData);
+                }
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                console.error(error);
+                setIsError(true);
+                setMessage("An error Occured. Try Again Later");
+            })
+    }, []);
 
     return (
         <View style={styles.container}>
+            {isLoading && <ActivityIndicator size="large" color="#04767a" />}
             {isError &&
                 <View style={styles.info}>
                     <Text style={styles.infoFont}>{message}</Text>
                 </View>
             }
-            {dataSet.map((test: PcrTest) => {
+            {pcrTestList.map((test: PcrTest) => {
                 return (
                     <TestResultCard key={test.id} testData={test} />
                 )
