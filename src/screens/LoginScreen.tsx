7eac76ddcebe135b175dde9e66bb2f4359aa8894
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2021-11-09 09:02:40 +0530

    Login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,5 @@
 import React, { memo, useState } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, ActivityIndicator} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Text, View, TouchableOpacity, StyleSheet, } from 'react-native';
 
 import CustomBackground from '../components/Layout/CustomBackground';
 import CustomTextInput from '../components/Layout/CustomTextInput';
@@ -11,7 +10,7 @@ import { theme } from '../shared/theme'
 import { emailValidator, passwordValidator } from '../shared/utils';
 
 import { Navigation } from '../constants/types';
-import {API_URL} from '../shared/routes';
+import { API_URL } from '../shared/routes';
 
 type Props = {
     navigation: Navigation;
@@ -26,18 +25,6 @@ const LoginScreen = ({ navigation }: Props) => {
     const [isError, setIsError] = useState(false);
     const [message, setMessage] = useState('');
     const [isLogin, setIsLogin] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const STORAGE_KEY_EMAIL = 'USER_EMAIL';
-    const STORAGE_KEY_USERID = 'USER_ID';
-
-    const saveUser = async (email:string , userID:string) => {
-        try {
-          await AsyncStorage.setItem(STORAGE_KEY_EMAIL, email);
-          await AsyncStorage.setItem(STORAGE_KEY_USERID, userID);
-        } catch (e) {
-          console.error('Failed to save name.')
-        }
-      }
 
     const _onLoginPressed = () => {
         const emailError = emailValidator(email);
@@ -54,7 +41,6 @@ const LoginScreen = ({ navigation }: Props) => {
             password,
         };
 
-        setIsLoading(true);
         fetch(`${API_URL}/login`, {
             method: 'POST',
             headers: {
@@ -62,33 +48,28 @@ const LoginScreen = ({ navigation }: Props) => {
             },
             body: JSON.stringify(payload),
         })
-        .then(async res => { 
-            console.log(res);
-            try {
-                const jsonRes = await res.json();
-                if (res.status !== 200) {
-                    setIsLoading(false);
-                    console.log("Error occured");
-                    console.log(jsonRes);
-                    setIsError(true);
-                    setMessage(jsonRes.exception);
-                } else {
-                    setIsLoading(false);
-                    console.log(jsonRes);
-                    saveUser(jsonRes.email, jsonRes.id);
-                    setIsError(false);
-                    setMessage('');
-                    navigation.navigate('UserRoot');
-                }
-            } catch (err) {
-                setIsLoading(false);
+            .then(async res => {
+                try {
+                    const jsonRes = await res.json();
+                    if (res.status !== 200) {
+                        console.log("Error occured");
+                        console.log(jsonRes);
+                        setIsError(true);
+                        setMessage(jsonRes.exception);
+                    } else {
+                        //onLoggedIn(jsonRes.token);
+                        console.log("No error");
+                        setIsError(false);
+                        setMessage(jsonRes.exception);
+                        navigation.navigate('UserRoot');
+                    }
+                } catch (err) {
+                    console.log(err);
+                };
+            })
+            .catch(err => {
                 console.log(err);
-            };
-        })
-        .catch(err => {
-            setIsLoading(false);
-            console.log(err);
-        });
+            });
     };
 
     return (
@@ -137,7 +118,6 @@ const LoginScreen = ({ navigation }: Props) => {
                 </TouchableOpacity>
             </View>
 
-            {isLoading && <ActivityIndicator size="large" color="#04767a" /> }
             {isError && <Text style={styles.errorText}>{message}</Text>}
 
             <CustomButton mode="contained" onPress={_onLoginPressed}>
